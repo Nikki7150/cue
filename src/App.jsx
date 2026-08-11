0c8bb17970f9992b9be40d567a529cc9c948532f
Nikki7150 <kanchan.nishka@gmail.com>
2026-08-10 23:05:45 -0700

    added protectedroute

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import DeckList from './pages/DeckList';
 import Login from './pages/Login';
 import Upload from './pages/Upload';
 import Review from './pages/Review';
+import { useAuth } from './AuthContext'
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { user, loading } = useAuth();
 
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<h1>Welcome to the App</h1>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/review" element={<Review />} />
-        <Route path="/decks" element={<DeckList />} />
+        <Route path="/upload" element={<ProtectedRoute><Upload /></ProtectedRoute>} />
+        <Route path="/review" element={<ProtectedRoute><Review /></ProtectedRoute>} />
+        <Route path="/decks" element={<ProtectedRoute><DeckList /></ProtectedRoute>} />
       </Routes>
     </div>
   )
