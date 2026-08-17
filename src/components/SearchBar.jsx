d3671fb703be3181cc4be1934509d3919d2f5543
Nikki7150 <kanchan.nishka@gmail.com>
2026-08-17 00:17:02 -0700

    added clear search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,26 @@
-import { IoSearchOutline } from "react-icons/io5";
+import { IoSearchOutline, IoCloseOutline } from "react-icons/io5";
+import { FaTimes } from "react-icons/fa";
 import '../styles/SearchBar.css';
 
 const SearchBar = ({ value, onChange}) => {
+    const handleClear = () => {
+
+    }
     return (
-        <div className="search-bar">
-            <IoSearchOutline size={20} />
-            <input 
-                type="text"
-                placeholder="Search decks and cards..."
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
-            />
+        <div className="search-bar-container">
+            <div className="search-bar">
+                <IoSearchOutline size={20} />
+                <input 
+                    type="text"
+                    placeholder="Search decks and cards..."
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                    className="search-input"
+                />
+                {value && (
+                    <button type="button" className="clear-button" onClick={() => onChange('')}><IoCloseOutline /></button>
+                )}
+            </div>
         </div>
     );
 };
